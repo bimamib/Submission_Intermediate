683e1e88da32459dd18e7f07a0c7a1657535d288
bimamib <bimaprast1601@gmail.com>
2023-10-12 03:08:25 +0700

    add custom edit text

--- app/src/main/java/com/bima/mystoryapp/CustomEditText.kt
+++ app/src/main/java/com/bima/mystoryapp/CustomEditText.kt
@@ -15,28 +15,33 @@ class CustomEditText : AppCompatEditText {
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         init()
     }
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
         init()
     }
 
     private fun init() {
+
         addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
                 // Do nothing.
             }
+
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.toString().length < 8) {
+                    setError("Password tidak boleh kurang dari 8 karakter", null)
+                } else {
+                    error = null
+                }
             }
+
             override fun afterTextChanged(s: Editable) {
                 // Do nothing.
             }
         })
-    }
-
-    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-        if (s.toString().length < 8) {
-            setError("Password tidak boleh kurang dari 8 karakter", null)
-        } else {
-            error = null
-        }
     }
 }
