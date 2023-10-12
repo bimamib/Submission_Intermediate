aa3b087d3df11a50603afddd6c1e8b18a55df67c
bimamib <bimaprast1601@gmail.com>
2023-10-12 18:33:08 +0700

    adding packages, moving files to each package, adding apps icons, adding icons logout, adding ApiService files, RegisterResponse

---
+++ app/src/main/java/com/bima/mystoryapp/data/remote/retrofit/ApiService.kt
@@ -0,0 +1,16 @@
+package com.bima.mystoryapp.data.remote.retrofit
+
+import com.bima.mystoryapp.data.response.RegisterResponse
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface ApiService {
+    @FormUrlEncoded
+    @POST("register")
+    suspend fun register(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): RegisterResponse
+}
